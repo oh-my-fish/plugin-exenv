326293b5d42152bbb0a2a9c7e29922e6bace72c1
Douglas Camata <d.camata@gmail.com>
2016-06-03 01:48:00 -0300

    improves exenv detection (closes #1)
    Not using the `available` statement anymore, because it is deprecated.
    
    Doesn’t stop the fish-shell loading workflow when exenv is not
    installed. Or other words: oh-my-fish will not stop loading if exenv is
    not available, for example.
    
    Also some “refactoring” for the warning message when exenv is not
    installed.

--- exenv.fish
+++ exenv.fish
@@ -2,8 +2,8 @@
 #   exenv [options]
 #
 function init --on-event init_exenv
-  if not available exenv
-    echo "Please install 'exenv' first!"; return 1
+  if not type --quiet "exenv"
+    echo "WARNING: you loaded the fish-shell plugin for exenv, but exenv is not installed."
   end
   if status --is-interactive
     if exenv init - | grep --quiet "function"
