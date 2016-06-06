f6d1e94c1d2524c43dee8d6dc5fb97ff83998ac0
Douglas Camata <d.camata@gmail.com>
2016-06-06 15:26:04 -0300

    removing unused code and refactoring
    Removed the unused ndenv function and improved the conditional for
    evenv setup.

--- init.fish
+++ init.fish
@@ -3,28 +3,13 @@
 #
 if not type --quiet "exenv"
   echo "WARNING: you loaded the fish-shell plugin for exenv, but exenv is not installed."
-end
-
-if status --is-interactive
+else if status --is-interactive
   if exenv init - | grep --quiet "function"
     source (exenv init - | psub)
   else
-
     if not set -q EXENV_ROOT
       set -x EXENV_ROOT "$HOME/.exenv"
     end
-
     set PATH "$EXENV_ROOT/shims" $PATH
-
-    function ndenv -d "Elixir version manager"
-      set command $argv[1]
-      set -e argv[1]
-      switch "$command"
-        case shell
-          eval (ndenv "sh-$command" $argv)
-        case '*'
-          command ndenv "$command" $argv
-      end
-    end
   end
 end
